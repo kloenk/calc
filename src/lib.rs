87d79f11f0b537a383c870b18ed063a06082854b
Finn Behrens <me@kloenk.de>
2020-01-13 21:36:21 +0100

    <insert commit message>

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,144 @@
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Operator {
-  Add,
-  Sub,
-  Mul,
-  Div,
-  Int(isize),
-  Float(f64),
-  Print,
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Int(isize),
+    Float(f64),
+    Print,
+}
+
+impl Operator {
+    fn isNumber(&self) -> bool {
+      match self {
+          Operator::Int(_) | Operator::Float(_) => true,
+          _ => false,
+      }
+    }
+
+    fn number(&self) -> f64 {
+      match self {
+        Operator::Int(v) => *v as f64,
+        Operator::Float(v) => *v,
+        _ => 0.0,
+      }
+    }
 }
 
 impl std::str::FromStr for Operator {
-  type Err = String;
+    type Err = String;
 
-  fn from_str(s: &str) -> Result<Operator, Self::Err> {
-    let s = match s {
-        "+" => Operator::Add,
-        "-" => Operator::Sub,
-        "/" => Operator::Div,
-        "*" => Operator::Mul,
-        "p" => Operator::Print,
-        _ => {
-          let s: Operator = if let Ok(s) = isize::from_str(s) {
-            Operator::Int(s)
-          } else {
-              f64::from_str(s).map(|v| Operator::Float(v)).unwrap()
-          };
-          println!("et {:?}", s);
-          s
+    fn from_str(s: &str) -> Result<Operator, Self::Err> {
+        let s = match s {
+            "+" => Operator::Add,
+            "-" => Operator::Sub,
+            "/" => Operator::Div,
+            "*" => Operator::Mul,
+            "p" => Operator::Print,
+            _ => {
+                let s: Operator = if let Ok(s) = isize::from_str(s) {
+                    Operator::Int(s)
+                } else {
+                    f64::from_str(s).map(|v| Operator::Float(v)).unwrap()
+                };
+                s
+            }
+        };
+        Ok(s)
+    }
+}
+
+#[derive(Debug)]
+pub struct Calculator {}
+
+impl Calculator {
+    pub fn exe(s: &mut Vec<Operator>) -> Result<(), String> {
+        let exe = match s.pop() {
+            Some(s) => s,
+            None => return Err("utraen".to_string()),
+        };
+
+        match exe {
+            Operator::Print => {
+                println!("{:?}", s);
+                Ok(())
+            }
+            Operator::Int(_) | Operator::Float(_) => {
+              s.push(exe);
+              Ok(())
+            }
+            Operator::Add => {
+              if s.len() < 2 {
+                return Err("aeuea".to_string());
+              }
+              let s1 = s.pop().unwrap();
+              let s2 = s.pop().unwrap();
+
+              if !s1.isNumber() || !s2.isNumber() {
+                return Err("aeuaea".to_string());
+              }
+
+              let s1 = s1.number();
+              let s2 = s2.number();
+
+              s.push(Operator::Float(s1 + s2));
+              Ok(())
+            }
+            Operator::Sub => {
+              if s.len() < 2 {
+                return Err("aeuea".to_string());
+              }
+              let s1 = s.pop().unwrap();
+              let s2 = s.pop().unwrap();
+
+              if !s1.isNumber() || !s2.isNumber() {
+                return Err("aeuaea".to_string());
+              }
+
+              let s1 = s1.number();
+              let s2 = s2.number();
+
+              s.push(Operator::Float(s2 - s1));
+              Ok(())
+            }
+            Operator::Div => {
+              if s.len() < 2 {
+                return Err("aeuea".to_string());
+              }
+              let s1 = s.pop().unwrap();
+              let s2 = s.pop().unwrap();
+
+              if !s1.isNumber() || !s2.isNumber() {
+                return Err("aeuaea".to_string());
+              }
+
+              let s1 = s1.number();
+              let s2 = s2.number();
+
+              s.push(Operator::Float(s2 / s1));
+              Ok(())
+            }
+            Operator::Mul => {
+              if s.len() < 2 {
+                return Err("aeuea".to_string());
+              }
+              let s1 = s.pop().unwrap();
+              let s2 = s.pop().unwrap();
+
+              if !s1.isNumber() || !s2.isNumber() {
+                return Err("aeuaea".to_string());
+              }
+
+              let s1 = s1.number();
+              let s2 = s2.number();
+
+              s.push(Operator::Float(s2 * s1));
+              Ok(())
+            }
+            _ => todo!(),
         }
-    };
-    Ok(s)
-  }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::prelude::*;
 use std::io;
+use std::io::prelude::*;
 
 use std::str::FromStr;
 
@@ -7,19 +7,25 @@ use calc;
 
 fn main() {
     let mut input = String::new();
+    let mut foo = Vec::new();
     loop {
         input.clear();
         print!(">> ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).expect("foo");
         let input = input.trim();
-        
-        //for v in input.split_whitespace() {
-        //    println!("{}", v);
-        //}
-        let input: Vec<calc::Operator> = input.split_whitespace().map(|v| { calc::Operator::from_str(v).unwrap() } ).collect();
 
-        println!("{:?}", input);
+        for v in input.split_whitespace() {
+            foo.push(calc::Operator::from_str(v).unwrap());
+            calc::Calculator::exe(&mut foo).unwrap();
+        }
+        //let mut foo: Vec<calc::Operator> = input
+        //    .split_whitespace()
+        //    .map(|v| calc::Operator::from_str(v).unwrap())
+        //    .collect();
+
+        
+        //println!("{:?}", input);
     }
 
     //println!("{:?}", input);
